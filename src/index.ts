e9651fffa28b9fe04324a5c0a21d1d7f9afb56f6
Partur-dev <parturtwo@gmail.com>
2023-02-02 05:08:33 +0200

    :kafif:

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,61 @@
-import { Injector, Logger, webpack } from "replugged";
+import { webpack } from "replugged";
+import "./style.css";
 
-const inject = new Injector();
-const logger = Logger.plugin("PluginTemplate");
+interface Message {
+  id: string;
+  channelId: string;
+}
+
+let enabled = false;
 
 export async function start(): Promise<void> {
-  const typingMod = await webpack.waitForModule<{
-    startTyping: (channelId: string) => void;
-  }>(webpack.filters.byProps("startTyping"));
-  const getChannelMod = await webpack.waitForModule<{
-    getChannel: (id: string) => {
-      name: string;
-    };
-  }>(webpack.filters.byProps("getChannel"));
+  enabled = true;
+  const deleted: Message[] = [];
 
-  if (typingMod && getChannelMod) {
-    inject.instead(typingMod, "startTyping", ([channel]) => {
-      const channelObj = getChannelMod.getChannel(channel);
-      logger.log(`Typing prevented! Channel: #${channelObj?.name ?? "unknown"} (${channel}).`);
-    });
+  const registerMod = await webpack.waitForModule<{
+    _actionHandlers: {
+      _orderedActionHandlers: {
+        MESSAGE_DELETE?: {
+          actionHandler: (msg: Message) => void;
+        }[];
+      };
+    };
+  }>(webpack.filters.byProps("register"));
+
+  const messageDelete = registerMod._actionHandlers._orderedActionHandlers.MESSAGE_DELETE;
+
+  if (messageDelete == undefined) {
+    setTimeout(start, 1000);
+    return;
   }
+
+  const handler = messageDelete!.find((x) =>
+    x.actionHandler.toString().includes("revealedMessageId"),
+  )!;
+
+  const origActionHandler = handler.actionHandler;
+
+  const applyStyles = (msg: Message) => {
+    const el = document.querySelector(`#chat-messages-${msg.channelId}-${msg.id}`)!;
+    if (el.classList.contains("deleted")) return;
+
+    if (el.querySelector("ephemeral-2nDdnn")) {
+      return origActionHandler(msg);
+    }
+
+    el.classList.add("deleted");
+  };
+
+  handler.actionHandler = (msg) => {
+    if (!enabled) return origActionHandler(msg);
+    deleted.push(msg);
+  };
+
+  setInterval(() => {
+    for (const msg of deleted) applyStyles(msg);
+  }, 500);
 }
 
 export function stop(): void {
-  inject.uninjectAll();
+  enabled = false;
 }
